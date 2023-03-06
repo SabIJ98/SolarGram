68682fdedd47d66c14199cf187e63cbf6aabd96c
SabIJ98 <127075824+SabIJ98@users.noreply.github.com>
2023-03-05 22:34:19 -0700

    Initial Commit

---
+++ SolarGram/SolarGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramApp.swift
+//  SolarGram
+//
+//  Created by Sabrina Jaen on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
